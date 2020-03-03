92592b396770a6d16175e246d3d62a7da9066555
Alejandro Elí <dev@onerbs.com>
2020-03-03 15:58:51 -0600

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import React    from 'react';
+import App      from './App.jsx';
 
-import App from './App.jsx';
-
-import './index.css';
-
-ReactDOM.render(<App />, document.body);
+ReactDOM.render(<App/>, document.body);
