0453770105ba5dcf72ad3f19f5cffdd42e316c1d
Alejandro Elí <dev@onerbs.com>
2020-03-06 00:47:11 -0600

    Improve AddUser component

--- src/AddUser.jsx
+++ src/AddUser.jsx
@@ -1,29 +1,34 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import { TextField, Button } from '@material-ui/core'
 import AddIcon from '@material-ui/icons/Add'
 
-export default class AddUser extends Component {
+export default class AddUser extends React.Component {
   state = {
-    value: ''
+    input: null
+  }
+  componentDidMount() {
+    this.setState({ input: document.getElementsByTagName('input')[0] })
   }
   addUser = () => {
-    this.props.addUser(this.state.value)
-    this.setState({ value: '' })
+    let input_ = this.state.input
+    this.props.addUser(input_.value)
+    input_.value = ''
+    this.setState({ input: input_ })
+    input_.focus()
   }
   render() {
     return (
       <div id="AddUser">
         <TextField
           label='Nombre'
-          value={this.state.value}
-          onChange={event => { this.setState({ value: event.target.value }) }}
-          onKeyUp={event => { if (event.key === 'Enter') this.addUser() }}/>
+          onChange={ev => { this.setState({ value: ev.target.value }) }}
+          onKeyUp={ev => { if (ev.key === 'Enter') { this.addUser() } }}/>
         <Button
           variant="contained"
           color="primary"
           startIcon={<AddIcon />}
-          onClick={this.addUser}>
+          onClick={_ => { this.addUser() }}>
           Añadir
         </Button>
       </div>
