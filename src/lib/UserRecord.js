ae017c777f036a69a52819bd739b6fd7b4634d17
Alejandro Elí <dev@onerbs.com>
2020-03-11 20:57:03 -0600

    Update UserRecord.js

---
+++ src/lib/UserRecord.js
@@ -0,0 +1,22 @@
+
+import R from './Random'
+
+export default class Record {
+  constructor(user) {
+    this.id = R.getString(15)
+    this.name = `${user.name.first} ${user.name.last}`
+    this.email = user.email.split('@')[0]
+    this.domain = user.email.split('@')[1]
+    this.avatar = user.picture.medium
+  }
+  rename = name => {
+    this.name = name
+    return this
+  }
+  static from = raw => new Record(raw)
+  static named = name => Record.random().then(r => r.rename(name))
+  static random = () =>
+    fetch('https://randomuser.me/api/')
+      .then(res => res.json())
+      .then(json => Record.from(json.results[0]))
+}
